d0ecc4e822cd2c79d1eb0c73cc3e15df2b8f998d
viethung0202 <laiviethung2003@gmail.com>
2026-05-13 13:43:38 +0700

    config cors

--- src/index.js
+++ src/index.js
@@ -27,7 +27,12 @@ cloudinary.config({
 });
 
 // ====================== MIDDLEWARE ======================
-app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
